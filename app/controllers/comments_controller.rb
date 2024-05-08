f206931a5ad71604fda37b2245b4e3ac52c49163
Nobuta Taiki <66196738+taikiwish@users.noreply.github.com>
2024-05-08 15:36:50 +0900

    コメント空状態の保存について

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
-    if @comment.save
+    if @comment.content.present? && @comment.save
       redirect_to prototype_path(@comment.prototype) # 今回の実装には関係ありませんが、このようにPrefixでパスを指定することが望ましいです。
     else
       @prototype = @comment.prototype
